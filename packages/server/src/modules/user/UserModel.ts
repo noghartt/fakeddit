aa2d998c92b3120056d4df765d32c5c385f9f2d8
Guilherme Ananias <iamguilhermeananias@gmail.com>
2021-09-12 19:28:48 -0300

    feat(users): write initial user model

---
+++ packages/server/src/modules/user/UserModel.ts
@@ -0,0 +1,72 @@
+import { Schema, model } from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+// TODO There's a way to remove methods from this interface?
+interface User {
+  username: string;
+  displayName: string;
+  email: string;
+  password: string;
+  hashPassword: (password: string) => Promise<string>;
+  comparePassword: (password: string) => string;
+}
+
+const UserSchema = new Schema<User>(
+  {
+    username: {
+      type: String,
+      required: true,
+      immutable: true,
+      unique: true,
+      minlength: 3,
+      maxlength: 25,
+    },
+    displayName: {
+      type: String,
+      required: false,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: true,
+      updatedAt: true,
+    },
+  },
+);
+
+UserSchema.pre('save', async function encryptPassword(next) {
+  if (!this.isModified('password') || !this.isNew) return next();
+
+  try {
+    const hashedPassword = await this.hashPassword(this.password);
+    this.password = hashedPassword;
+  } catch (err: any) {
+    // TODO Change the type of this `err` from catch
+    throw new Error(err);
+  }
+});
+
+UserSchema.methods = {
+  hashPassword: async function (password) {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(password, salt);
+
+      return hash;
+    } catch (err: any) {
+      // TODO Change the type of this `err` from catch
+      throw new Error(err);
+    }
+  },
+};
+
+export const UserModel = model('User', UserSchema);
