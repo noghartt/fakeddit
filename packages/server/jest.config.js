868ac18478640fcd327e98a62b2317f4aa7408c4
Guilherme Ananias <iamguilhermeananias@gmail.com>
2021-11-10 22:22:44 -0300

    test(jest): add a babel-transformer on jest config

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -8,6 +8,9 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', './dist'],
   coverageReporters: ['lcov', 'html'],
   resetModules: false,
+  transform: {
+    '^.+\\.(js|ts|tsx)?$': '<rootDir>/test/babel-transformer',
+  },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts|tsx)?$',
   moduleFileExtensions: ['ts', 'js', 'tsx', 'json'],
 };
