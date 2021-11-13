52f9fa734fcec74d4f06c11ab90ddc6ac1dc0f78
Guilherme Ananias <iamguilhermeananias@gmail.com>
2021-11-13 11:53:06 -0300

    test(jest): remove unnecessary presets property from jest root config

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,4 @@
 module.exports = {
   projects: ['<rootDir>/packages/server/jest.config.js'],
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
   moduleFileExtensions: ['js', 'ts', 'tsx'],
 };
