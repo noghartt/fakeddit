1ff25cf1895623901053a1e6a1a6b6eee57bd427
Guilherme Ananias <iamguilhermeananias@gmail.com>
2021-11-22 15:23:44 -0300

    feat: add ChakraProvider to App root file

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,1 +1,9 @@
-export const App = () => <h1>Hello, world!</h1>;
+import { ChakraProvider } from '@chakra-ui/react';
+
+import { theme } from '@fakeddit/ui';
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <h1>Hello, world</h1>
+  </ChakraProvider>
+);
