ccae22455c707cd12058c08de42ca2a83ac20a50
Guilherme Ananias <iamguilhermeananias@gmail.com>
2021-11-29 21:10:54 -0300

    style: rename the extension from generate schema file to .graphql

--- packages/server/scripts/generateSchema.ts
+++ packages/server/scripts/generateSchema.ts
@@ -7,11 +7,13 @@ import { schema } from '../src/schema';
 
 const pwd = process.cwd();
 
+const schemaFile = 'schema.graphql';
+
 const generateSchema = async () => {
-  const configs = [{ schema, path: path.join(pwd, './graphql/schema.gql') }];
+  const configs = [{ schema, path: path.join(pwd, './graphql', schemaFile) }];
 
   for (const config of configs) {
-    const dirPath = config.path.split('/schema.gql')[0];
+    const dirPath = config.path.split(schemaFile)[0];
 
     // TODO: Should I put it on a try/catch?
     if (!fsSync.existsSync(dirPath)) {
