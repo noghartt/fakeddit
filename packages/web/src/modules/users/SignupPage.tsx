362fd07f7c81c0939b34f372366b1659b7188add
Guilherme Ananias <iamguilhermeananias@gmail.com>
2021-11-30 19:02:35 -0300

    feat: import UserRegisterMutation on SignUp page and handle it

--- packages/web/src/modules/users/SignupPage.tsx
+++ packages/web/src/modules/users/SignupPage.tsx
@@ -1,14 +1,71 @@
-import { Input } from '@chakra-ui/react';
+import { Input, Spinner } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import { useMutation } from 'react-relay';
+import { Formik, Form, Field, FormikHelpers, FieldProps } from 'formik';
 
 import { VStack, Button } from '@fakeddit/ui';
 
-export const SignupPage = () => (
-  <VStack as="form" alignSelf="center" spacing="12px">
-    <Input type="email" placeholder="Email" />
-    <Input placeholder="Username" />
-    <Input placeholder="Password" type="password" />
-    <Button width="100%" type="submit">
-      Sign up
-    </Button>
-  </VStack>
-);
+import { useAuth } from '../auth/useAuth';
+
+import { UserRegister } from './UserRegisterMutation';
+import type { UserRegisterMutation } from './__generated__/UserRegisterMutation.graphql';
+
+const initialValues = {
+  username: '',
+  email: '',
+  password: '',
+};
+
+type InitialValues = typeof initialValues;
+type formikSubmitFn<T> = (values: T, formikHelpers: FormikHelpers<T>) => void;
+
+export const SignupPage = () => {
+  const { signin } = useAuth();
+  const navigate = useNavigate();
+
+  const [handleUserRegister] = useMutation<UserRegisterMutation>(UserRegister);
+
+  const handleSubmit: formikSubmitFn<InitialValues> = (values, actions) => {
+    handleUserRegister({
+      variables: values,
+      onCompleted: ({ userRegisterMutation }) => {
+        // TODO: Instead of it, could I resume this functions on actions.resetForm?
+        actions.setValues(initialValues);
+        actions.setSubmitting(false);
+
+        signin(userRegisterMutation?.token, () => {
+          navigate('/feed', { replace: true });
+        });
+      },
+    });
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      {({ isSubmitting }) => (
+        <Form>
+          <VStack alignSelf="center" spacing="12px">
+            <Field name="email">
+              {({ field }: FieldProps) => (
+                <Input {...field} type="email" placeholder="Email" />
+              )}
+            </Field>
+            <Field name="username">
+              {({ field }: FieldProps) => (
+                <Input {...field} placeholder="Username" />
+              )}
+            </Field>
+            <Field name="password">
+              {({ field }: FieldProps) => (
+                <Input {...field} type="password" placeholder="Password" />
+              )}
+            </Field>
+            <Button width="100%" textTransform="uppercase" type="submit">
+              {isSubmitting ? <Spinner /> : 'Sign up'}
+            </Button>
+          </VStack>
+        </Form>
+      )}
+    </Formik>
+  );
+};
