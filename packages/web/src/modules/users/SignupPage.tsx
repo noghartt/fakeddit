fd42d028233e4969f9c4b377caa9f13eee324c93
Guilherme Ananias <iamguilhermeananias@gmail.com>
2021-12-03 21:49:34 -0300

    feat: add signupSchema and validation to form on SignUp page

--- packages/web/src/modules/users/SignupPage.tsx
+++ packages/web/src/modules/users/SignupPage.tsx
@@ -1,9 +1,12 @@
-import { Input, Spinner } from '@chakra-ui/react';
+import { Spinner } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 import { useMutation } from 'react-relay';
-import { Formik, Form, Field, FormikHelpers, FieldProps } from 'formik';
+import { Formik, Form, FormikHelpers } from 'formik';
+import * as Yup from 'yup';
 
 import { VStack, Button } from '@fakeddit/ui';
+
+import { InputField } from '@/shared-components/InputField';
 
 import { useAuth } from '../auth/useAuth';
 
@@ -15,6 +18,18 @@ const initialValues = {
   email: '',
   password: '',
 };
+
+const signupSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Email is required'),
+  username: Yup.string()
+    .min(3, 'The username needs at least 3 characters')
+    .max(25)
+    .required('Username is required'),
+  // TODO: The rules to write a safe password need to be improved adding some
+  password: Yup.string()
+    .min(8, 'The password needs at least 8 characters')
+    .required('Password is required'),
+});
 
 type InitialValues = typeof initialValues;
 type formikSubmitFn<T> = (values: T, formikHelpers: FormikHelpers<T>) => void;
@@ -41,26 +56,34 @@ export const SignupPage = () => {
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-      {({ isSubmitting }) => (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={signupSchema}
+      onSubmit={handleSubmit}
+      validateOnMount
+    >
+      {({ isSubmitting, isValid }) => (
         <Form>
           <VStack alignSelf="center" spacing="12px">
-            <Field name="email">
-              {({ field }: FieldProps) => (
-                <Input {...field} type="email" placeholder="Email" />
-              )}
-            </Field>
-            <Field name="username">
-              {({ field }: FieldProps) => (
-                <Input {...field} placeholder="Username" />
-              )}
-            </Field>
-            <Field name="password">
-              {({ field }: FieldProps) => (
-                <Input {...field} type="password" placeholder="Password" />
-              )}
-            </Field>
-            <Button width="100%" textTransform="uppercase" type="submit">
+            <InputField
+              name="email"
+              type="email"
+              placeholder="Email"
+              shouldValidate
+            />
+            <InputField name="username" placeholder="Username" shouldValidate />
+            <InputField
+              name="password"
+              type="password"
+              placeholder="Password"
+              shouldValidate
+            />
+            <Button
+              width="100%"
+              textTransform="uppercase"
+              type="submit"
+              disabled={!isValid}
+            >
               {isSubmitting ? <Spinner /> : 'Sign up'}
             </Button>
           </VStack>
