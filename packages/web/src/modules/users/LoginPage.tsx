da374b2fd304587123e5b3de1ac6a8c2394fb6f1
Guilherme Ananias <iamguilhermeananias@gmail.com>
2021-12-08 23:17:04 -0300

    feat: add ErrorMessage and useFormik on form inside LoginPage

--- packages/web/src/modules/users/LoginPage.tsx
+++ packages/web/src/modules/users/LoginPage.tsx
@@ -1,10 +1,11 @@
+import { useState } from 'react';
 import { Spinner } from '@chakra-ui/react';
 import { useMutation } from 'react-relay';
 import { useNavigate } from 'react-router-dom';
-import { Formik, Form, FormikHelpers } from 'formik';
+import { useFormik, FormikProvider, Form } from 'formik';
 import * as Yup from 'yup';
 
-import { VStack, Button } from '@fakeddit/ui';
+import { VStack, Button, ErrorText } from '@fakeddit/ui';
 
 import { InputField } from '@/shared-components/InputField';
 
@@ -13,66 +14,67 @@ import { useAuth } from '../auth/useAuth';
 import { UserLogin } from './UserLoginMutation';
 import type { UserLoginMutation } from './__generated__/UserLoginMutation.graphql';
 
-const loginSchema = Yup.object().shape({
-  username: Yup.string()
-    .min(3, 'The username needs at least 3 characters')
-    .max(25)
-    .required('Username is required'),
-  // TODO: The rules to write a safe password need to be improved adding some
-  password: Yup.string()
-    .min(8, 'The password needs at least 8 characters')
-    .required('Password is required'),
-});
-
-const initialValues = { username: '', password: '' };
-
-type formikSubmitFn<T> = (values: T, formikHelpers: FormikHelpers<T>) => void;
-
 export const LoginPage = () => {
   const { signin } = useAuth();
   const navigate = useNavigate();
 
+  const [error, setError] = useState({
+    status: false,
+    message: '',
+  });
+
   const [handleUserLogin] = useMutation<UserLoginMutation>(UserLogin);
 
-  const handleSubmit: formikSubmitFn<typeof initialValues> = (
-    values,
-    actions,
-  ) => {
-    handleUserLogin({
-      variables: values,
-      onCompleted: ({ userLoginMutation }) => {
-        actions.setValues(initialValues);
-        actions.setSubmitting(false);
+  const formikValue = useFormik({
+    initialValues: { username: '', password: '' },
+    isInitialValid: false,
+    validationSchema: Yup.object().shape({
+      username: Yup.string()
+        .min(3, 'The username needs at least 3 characters')
+        .max(25)
+        .required('Username is required'),
+      // TODO: The rules to write a safe password need to be improved adding some
+      password: Yup.string()
+        .min(8, 'The password needs at least 8 characters')
+        .required('Password is required'),
+    }),
+    onSubmit: (values, actions) => {
+      handleUserLogin({
+        variables: values,
+        onCompleted: ({ userLoginMutation }, error) => {
+          actions.resetForm();
 
-        signin(userLoginMutation?.token, () => {
-          navigate('/feed', { replace: true });
-        });
-      },
-    });
-  };
+          if (error && error.length > 0) {
+            setError({ status: true, message: error[0].message });
+          } else {
+            signin(userLoginMutation?.token, () => {
+              navigate('/feed', { replace: true });
+            });
+          }
+        },
+      });
+    },
+  });
+
+  const { isValid, isSubmitting } = formikValue;
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={loginSchema}
-      onSubmit={handleSubmit}
-      validateOnMount
-    >
-      {({ isSubmitting, isValid }) => (
-        <Form>
-          <VStack alignSelf="center" spacing="12px">
-            <InputField name="username" placeholder="Username" />
-            <InputField
-              name="password"
-              placeholder="Password"
-              type="password"
-            />
-            <Button width="100%" type="submit" disabled={!isValid}>
-              {isSubmitting ? <Spinner /> : 'Log in'}
-            </Button>
-          </VStack>
-        </Form>
-      )}
-    </Formik>
+    <FormikProvider value={formikValue}>
+      <Form>
+        <VStack alignSelf="center" spacing="12px">
+          <InputField name="username" placeholder="Username" shouldValidate />
+          <InputField
+            name="password"
+            placeholder="Password"
+            type="password"
+            shouldValidate
+          />
+          <Button type="submit" disabled={!isValid}>
+            {isSubmitting ? <Spinner /> : 'Log in'}
+          </Button>
+          {error.status && <ErrorText>{error.message}</ErrorText>}
+        </VStack>
+      </Form>
+    </FormikProvider>
   );
 };
