7dd32900ee17c0a0334a8b04773d5f54567c89fd
Guilherme Ananias <iamguilhermeananias@gmail.com>
2021-12-15 00:23:16 -0300

    test: write tests to createCommunityMutation

---
+++ packages/server/src/modules/community/__tests__/createCommunity.test.ts
@@ -0,0 +1,133 @@
+import { graphql } from 'graphql';
+
+import {
+  clearDatabaseAndRestartCounters,
+  connectWithMongoose,
+  disconnectWithMongoose,
+} from '../../../../test';
+import { schema } from '../../../schema';
+
+import { createUser } from '../../user/fixtures/createUser';
+
+beforeAll(connectWithMongoose);
+beforeEach(clearDatabaseAndRestartCounters);
+afterAll(disconnectWithMongoose);
+
+it('should create a new community', async () => {
+  const user = await createUser();
+
+  const mutation = `
+    mutation M($displayName: String!, $communityId: String!) {
+      createCommunityMutation(
+        input: { displayName: $displayName, communityId: $communityId }
+      ) {
+        community {
+          id
+          name
+          displayName
+          members
+        }
+      }
+    }
+  `;
+
+  const rootValue = {};
+
+  const variables = {
+    displayName: 'A community to lovers of tests',
+    communityId: 'WeLoveTests',
+  };
+
+  const result = await graphql(
+    schema,
+    mutation,
+    rootValue,
+    { user },
+    variables,
+  );
+
+  expect(result.errors).toBeUndefined();
+
+  const { community } = result?.data?.createCommunityMutation;
+
+  expect(community.id).toBeDefined();
+  expect(community.name).toBe(variables.communityId);
+  expect(community.displayName).toBe(variables.displayName);
+  expect(community.members).toHaveLength(1);
+  expect(community.members).toContain(user._id.toString());
+});
+
+it("should not allow create a community if doesn't have authorization header", async () => {
+  const mutation = `
+    mutation M($displayName: String!, $communityId: String!) {
+      createCommunityMutation(
+        input: { displayName: $displayName, communityId: $communityId }
+      ) {
+        community {
+          id
+          name
+          displayName
+          members
+        }
+      }
+    }
+  `;
+
+  const rootValue = {};
+
+  const variables = {
+    displayName: 'A community to lovers of tests',
+    communityId: 'WeLoveTests',
+  };
+
+  const result = await graphql(schema, mutation, rootValue, {}, variables);
+
+  expect(result?.data?.createCommunityMutation).toBeNull();
+
+  expect(result?.errors).toBeDefined();
+  expect(result.errors && result.errors[0]?.message).toBe(
+    'You are not logged in. Please, try again!',
+  );
+});
+
+it('should not create a duplicate community', async () => {
+  const user = await createUser();
+
+  const mutation = `
+    mutation M($displayName: String!, $communityId: String!) {
+      createCommunityMutation(
+        input: { displayName: $displayName, communityId: $communityId }
+      ) {
+        community {
+          id
+          name
+          displayName
+          members
+        }
+      }
+    }
+  `;
+
+  const rootValue = {};
+
+  const variables = {
+    displayName: 'A community to lovers of tests',
+    communityId: 'WeLoveTests',
+  };
+
+  await graphql(schema, mutation, rootValue, { user }, variables);
+  const result = await graphql(
+    schema,
+    mutation,
+    rootValue,
+    { user },
+    variables,
+  );
+
+  expect(result?.data?.createCommunityMutation).toBeNull();
+
+  expect(result?.errors).toBeDefined();
+  expect(result.errors && result.errors[0].message).toBe(
+    'A community with this name has already been created. Please, try again!',
+  );
+});
