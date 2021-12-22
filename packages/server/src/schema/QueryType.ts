291616d28e3bd979f2db38b70ca9ec8e5d10bc8c
Guilherme Ananias <iamguilhermeananias@gmail.com>
2021-12-22 13:00:30 -0300

    feat: add node/nodes fields on QueryType

--- packages/server/src/schema/QueryType.ts
+++ packages/server/src/schema/QueryType.ts
@@ -1,11 +1,14 @@
 import { GraphQLObjectType } from 'graphql';
 
 import { version } from '../modules/common/queries';
+import { nodeField, nodesField } from '../modules/graphql/typeRegister';
 
 export const QueryType = new GraphQLObjectType({
   name: 'Query',
   description: 'Root of queries',
   fields: () => ({
+    node: nodeField,
+    nodes: nodesField,
     version,
   }),
 });
